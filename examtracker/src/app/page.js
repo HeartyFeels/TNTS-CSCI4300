00c9298dbab13bc1b1689da34b4177db02b6b613
tryphena-jk <taj30846@uga.edu>
2024-03-23 10:30:55 -0400

    Update page.js

--- examtracker/src/app/page.js
+++ examtracker/src/app/page.js
@@ -1,6 +1,7 @@
 import SignUpBox from "./SignUpBox"
 import Card from "./Card"
-
+import AddExam from "./AddExam"
+import EditExam from "./EditExam"
 export default function Home() {
   return ( 
     <section>
