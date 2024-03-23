8fbe6478de85281e3449954937023650f9cdc5ad
tryphena-jk <taj30846@uga.edu>
2024-03-23 10:28:07 -0400

    Update EditExam.js

--- examtracker/src/app/EditExam.js
+++ examtracker/src/app/EditExam.js
@@ -1,4 +1,18 @@
 const EditExam = () => {
+    return (
+        <div class="grid">
+            <h1>Add Exam</h1>
+            <label>Name of Exam:</label>
+            <input required type="text"/>
+            <label>Subject:</label>
+            <input required type="text"/>
+            <label>Date of Exam:</label>
+            <input required type="date"/>
+            <label>Image Address:</label>
+            <input type="text"/>
+            <button>Save Changes</button>
+        </div>
+    )
 
-    
 }
+export default EditExam;
