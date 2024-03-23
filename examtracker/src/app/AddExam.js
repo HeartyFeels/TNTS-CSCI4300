085bf542d006188da0fc3e28d1e00c55b285e9b0
tryphena-jk <taj30846@uga.edu>
2024-03-23 10:17:18 -0400

    Update AddExam.js

--- examtracker/src/app/AddExam.js
+++ examtracker/src/app/AddExam.js
@@ -1,4 +1,20 @@
 const AddExam = () => {
+    return (
+        <div class="card">
+            <h1>Add Exam</h1>
 
-    
+            <label>Name of Exam:</label>
+            <input required type="text"/>
+
+            <label>Subject:</label>
+            <input required type="text"/>
+
+            <label>Date of Exam:</label>
+            <input required type="text"/>
+
+            <label>Image Address:</label>
+            <input type="text"/>
+        </div>
+    )
 }
+export default AddExam;
