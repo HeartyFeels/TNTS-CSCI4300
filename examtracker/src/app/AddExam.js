05d1c7e78d4b7a530b7938beabfb9adb62507428
nivedhanatarajan7 <129971342+nivedhanatarajan7@users.noreply.github.com>
2024-03-23 11:55:00 -0400

    Added NavBar file

--- examtracker/src/app/AddExam.js
+++ examtracker/src/app/AddExam.js
@@ -2,22 +2,22 @@ import Row from "./Row";
 import Button from "./Button";
 
 let addExam = {
-    name: "Name of Exam:",
+    name: "Name of Exam: ",
     type: "text"
 }
 
 let subject = {
-    name: "Subject:",
+    name: "Subject: ",
     type: "text"
 }
 
 let dateOfExam = {
-    name: "Date of Exam:",
+    name: "Date of Exam: ",
     type: "date"
 }
 
 let imgAddress = {
-    name: "Image Address:",
+    name: "Image Address: ",
     type: "text"
 }
 
--- examtracker/src/app/page.js
+++ examtracker/src/app/page.js
@@ -2,11 +2,21 @@ import SignUpBox from "./SignUpBox"
 import Card from "./Card"
 import AddExam from "./AddExam"
 import EditExam from "./EditExam"
+import LoginBox from "./LoginBox"
+
 export default function Home() {
   return ( 
     <section>
       <Card>
         <SignUpBox />
+      </Card>
+
+      <Card>
+        <LoginBox />
+      </Card>
+
+      <Card>
+        <AddExam />
       </Card>
     </section>
   );
