c0ca8480d1c3d58d6a13ddc5406e6030feb75074
tryphena-jk <taj30846@uga.edu>
2024-04-18 16:51:24 -0400

    Update SignUpBox.js

--- examtracker/src/app/components/SignUpBox.js
+++ examtracker/src/app/components/SignUpBox.js
@@ -3,14 +3,32 @@ import Button from "./Button";
 import Card from "./Card";
 import Link from "next/link";
 
-// const User = require('../../models/User');
-// userRouter.post('/signup', async (req, res) => {
-//     try {
-//         const {firstName, lastName, username, password} = req.body;
-//     }
-// })
+const User = require('../../models/User');
 
 
+const submitHandlerSignIn = (event) => {
+    event.preventDefault();
+        userRouter.post('/signup', async (req, res) => {
+        try {
+            const {firstName, lastName, username, password} = req.body;
+            if (!firstName || !lastName || !username || !password) {
+                return res.status(400).json({msg: "Please enter all of the fields."});
+            }
+            if (password.length < 6) {
+                return res
+                    .status(400)
+                    .json({msg: "Password should be at least 6 characters"});
+            }
+            const hashedPassword = await bcryptjs.hash(password, 8);
+            const newUser = new User({firstName, lastName, username, password: hashedPassword});
+            const savedUser = await newUser.save();
+            
+            res.json(savedUser);
+        } catch (err) {
+            res.status(500).json({error: err.message});
+        }
+    });
+};
 
 
 
@@ -20,36 +38,38 @@ const SignUpBox = (props) => {
         <div>
             <Card>
                 <h1>Sign Up</h1>
-                <div className="row">
-                    <label>First Name: </label>
-                    <input 
-                    required type="text"
-                    />
-                </div>
+                <form onSubmit={submitHandlerSignIn}>
+                    <div className="row">
+                        <label>First Name: </label>
+                        <input 
+                        required type="text"
+                        />
+                    </div>
 
-                <div className="row">
-                    <label>Last Name: </label>
-                    <input 
-                    required type="text" 
-                    />
-                </div>
+                    <div className="row">
+                        <label>Last Name: </label>
+                        <input 
+                        required type="text" 
+                        />
+                    </div>
 
-                <div className="row">
-                    <label>Username: </label>
-                    <input 
-                    required type="text"
-                    />
-                </div>
+                    <div className="row">
+                        <label>Username: </label>
+                        <input 
+                        required type="text"
+                        />
+                    </div>
 
-                <div className="row">
-                    <label>Password: </label>
-                    <input 
-                    required type="password"
-                    />
-                </div>
+                    <div className="row">
+                        <label>Password: </label>
+                        <input 
+                        required type="password"
+                        />
+                    </div>
 
-                <Link href="/schedule" onClick= {props.setActive} ><Button>Sign Up</Button></Link>
-                <p id="option">Already have an account? Log in <Link href="/login">here</Link> </p>
+                    <Link href="/schedule"><Button type="submit">Sign Up</Button></Link>
+                </form>
+            <p id="option">Already have an account? Log in <Link href="/login">here</Link> </p>
             </Card>
         </div>
     );
